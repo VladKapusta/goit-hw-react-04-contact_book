64a990aa70abcec590cda4b5a6f8b33f9fe62d1a
VladKapusta <110594042+VladKapusta@users.noreply.github.com>
2023-02-18 15:24:17 +0200

    Initial commit

---
+++ src/components/ContactList/ContactItem/ContactItem.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import { IconButton } from 'components/Button/Button';
+
+export const TextContent = styled.span`
+  width: 250px;
+  font-weight: 600;
+  font-size: 24px;
+`;
+export const DeleteBtn = styled(IconButton)`
+  background-color: #fff;
+  :hover {
+    background-color: red;
+    fill: #fff;
+  }
+`;
---
+++ src/components/FormaAddContact/FormAddContact.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import {
+  FormContact,
+  Label,
+  LabelText,
+  Input,
+  ButtonSubmit,
+} from './FormAddContact.styled';
+
+export class FormAddContact extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  saveInputValue = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  onSubmitForm = e => {
+    const { name, number } = this.state;
+    e.preventDefault();
+    const contact = {
+      id: nanoid(5),
+      name,
+      number,
+    };
+    this.props.addContact(contact);
+    this.clearFormInput();
+    this.props.closeModal();
+  };
+
+  clearFormInput = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <FormContact onSubmit={this.onSubmitForm}>
+        <Label>
+          <LabelText>Name</LabelText>
+          <Input
+            autoFocus
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.saveInputValue}
+          />
+        </Label>
+        <Label>
+          <LabelText>Number</LabelText>
+          <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.saveInputValue}
+          />
+        </Label>
+        <ButtonSubmit type="submit">Add Contact</ButtonSubmit>
+      </FormContact>
+    );
+  }
+}
